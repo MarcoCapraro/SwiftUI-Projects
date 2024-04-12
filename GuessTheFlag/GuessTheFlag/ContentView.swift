415ba05b32a60bc9eb03ec6b179395d3d5b25474
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-04-12 14:10:13 -0400

    Stacking Practice
    Using VStacks, HStacks, and ZStacks to modify UI Elements

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,13 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            VStack(alignment: .center) {
+                HStack {
+                    Text("1")
+                    Text("2")
+                    Text("3")
+                }
+                HStack {
+                    Text("4")
+                    Text("5")
+                    Text("6")
+                }
+                HStack {
+                    Text("7")
+                    Text("8")
+                    Text("9")
+                }
+            }
+            
+            VStack(alignment: .center) {
+                HStack {
+                    Text("11")
+                    Text("2")
+                    Text("3")
+                }
+                HStack {
+                    Text("44")
+                    Text("5")
+                    Text("6")
+                }
+                HStack {
+                    Text("77")
+                    Text("8")
+                    Text("9")
+                }
+            }
         }
-        .padding()
     }
 }
 
