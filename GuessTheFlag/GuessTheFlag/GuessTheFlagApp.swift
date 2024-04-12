97433bbf21c8ba11fbeffba8024d0ce8cf6ea1d3
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-04-12 13:59:28 -0400

    Added the GuessTheFlag Project

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Marco Capraro on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
