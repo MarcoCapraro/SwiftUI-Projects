c305979120e140e370abf275fb0d7ad962d34900
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-04-17 16:27:43 -0400

    Complex View Hierarchies (@Viewbuilder)

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -10,8 +10,15 @@ import SwiftUI
 struct ContentView: View {
     @State private var isOn = false
     
+    @ViewBuilder var motto1: some View {
+        Text("The Golden Rule")
+    }
+    
+    let motto2 = Text("Honesty is the best policy")
+    
     var body: some View {
         VStack {
+            Spacer()
             Button("Hellow World") {
                 isOn.toggle()
             }
@@ -26,6 +33,10 @@ struct ContentView: View {
                 Text("Title1")
                 Text("Title2")
                 Text("Title3")
+                       
+                Spacer()
+                motto1
+                motto2
             }
             .font(.title)
         }
