d25485686cbd7b9bec822eb17a8eba82e9ca8064
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-04-23 16:13:08 -0400

    DatePicker Practice

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
     
     var body: some View {
         ZStack {
             LinearGradient(colors: [.red, .green, .blue], startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
             
-            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+            VStack {
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                DatePicker("Please Enter a Future Date:", selection: $wakeUp, in: Date.now...)
+                    .labelsHidden()
+            }
         }
     }
 }
