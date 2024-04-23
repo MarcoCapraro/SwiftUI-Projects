f3dd10a76c8774ad75474a9242e2e59d58542a98
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-04-23 16:05:04 -0400

    Steeper Practice

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -8,13 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    
     var body: some View {
         ZStack {
             LinearGradient(colors: [.red, .green, .blue], startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
             
-            Text("Hello, world!")
-                .foregroundStyle(.indigo)
+            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
         }
     }
 }
