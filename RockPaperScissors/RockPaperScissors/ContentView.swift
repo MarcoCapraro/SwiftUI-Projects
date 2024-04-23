d7554bd461d6bcf86f9ad980b086d5465f513595
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-04-23 12:01:54 -0400

    Refactoring and UI Customization for Rock Paper Scissors

--- RockPaperScissors/RockPaperScissors/ContentView.swift
+++ RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -7,10 +7,36 @@
 
 import SwiftUI
 
+struct RPSImage: View {
+    var sysString: String
+    
+    var body: some View {
+        Image(systemName: sysString)
+            .resizable()
+            .frame(maxWidth: 100, maxHeight: 100)
+    }
+}
+
+struct Title2: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.title2.weight(.semibold))
+            .foregroundStyle(.white)
+    }
+}
+
+extension View {
+    func title2Style() -> some View {
+        modifier(Title2())
+    }
+}
+
 struct ContentView: View {
     @State private var opponentHands = ["Rock", "Paper", "Scissors"].shuffled()
     @State private var randomHand = Int.random(in: 0...2)
     @State private var gameCondition = Bool.random()
+    @State private var showAlert = false
+    @State private var turn = 0
     @State private var score = 0
     
     let handImages = ["mountain.2.circle", "newspaper.circle", "scissors.circle"]
@@ -18,57 +44,76 @@ struct ContentView: View {
         
     func optionTapped(_ num: Int) {
         let opponentHand = opponentHands[randomHand]
-        var answeredCorrectly = false
+        let userChoice = handOptions[num]
+        let addPoint = isCorrect(with: userChoice, against: opponentHand)
+                
+        if(addPoint) {
+            score = min(score + 1, 10)
+        } else {
+            // do nothing
+            score = max(score - 1, 0)
+        }
         
+        turn = min(turn + 1, 10)
+        nextQuestion()
+    }
+    
+    func isCorrect(with choice: String, against hand: String) -> Bool {
+        var result: Bool = false
         // Determine winning option
         if(gameCondition) {
-            if(opponentHand == "Rock") {
-                answeredCorrectly = handOptions[num] == "Paper" ? true : false
-            } else if(opponentHand == "Paper") {
-                answeredCorrectly = handOptions[num] == "Scissors" ? true : false
+            if(hand == "Rock") {
+                result = choice == "Paper" ? true : false
+            } else if(hand == "Paper") {
+                result = choice == "Scissors" ? true : false
             } else {
-                answeredCorrectly = handOptions[num] == "Rock" ? true : false
+                result = choice == "Rock" ? true : false
             }
             
         // Determine losing option
         } else {
-            if(opponentHand == "Rock") {
-                answeredCorrectly = handOptions[num] == "Scissors" ? true : false
-            } else if(opponentHand == "Paper") {
-                answeredCorrectly = handOptions[num] == "Rock" ? true : false
+            if(hand == "Rock") {
+                result = choice == "Scissors" ? true : false
+            } else if(hand == "Paper") {
+                result = choice == "Rock" ? true : false
             } else {
-                answeredCorrectly = handOptions[num] == "Paper" ? true : false
+                result = choice == "Paper" ? true : false
             }
         }
-                
-        if(answeredCorrectly) {
-            score += 1
-        } else {
-            // do nothing
-        }
         
-        nextQuestion()
+        return result
     }
     
     func nextQuestion() {
         opponentHands = opponentHands.shuffled()
         randomHand = Int.random(in: 0...2)
-        gameCondition.toggle()
+        gameCondition = Bool.random()
+        
+        if(turn == 10) {
+            // Finish game by showing alert with score and reset
+            showAlert = true
+        }
+    }
+    
+    func restartGame() {
+        turn = 0
+        score = 0
+        
     }
     
     var body: some View {
         NavigationStack {
             ZStack {
-                Color.black
+                LinearGradient(colors: [.black, .red], startPoint: .center, endPoint: .bottom)
                     .ignoresSafeArea()
                 
                 VStack {
                     
                     Spacer()
-        
+                    
                     Text("Opponent Chooses: \(opponentHands[randomHand])")
-                        .font(.title2.weight(.semibold))
-                        .foregroundStyle(.white)
+                        .title2Style()
+                    
                     Text("What Hand **\(gameCondition ? "Wins" : "Loses")** this Turn?")
                         .font(.callout.weight(.light))
                         .foregroundStyle(.white)
@@ -79,28 +124,28 @@ struct ContentView: View {
                         ForEach(0..<3) { number in
                             ZStack {
                                 RadialGradient(colors: [.red, .black], center: .center, startRadius: 10, endRadius: 55)
-                            
+                                
                                 Button {
                                     // choice tapped
                                     optionTapped(number)
                                 } label: {
-                                    Image(systemName: handImages[number])
-                                        .resizable()
-                                        .frame(maxWidth: 100, maxHeight: 100)
+                                    RPSImage(sysString: handImages[number])
                                 }
                                 .foregroundStyle(.white)
                             }
                             .frame(maxWidth: 150, maxHeight: 150)
-                            
-                            
+                            .clipShape(.circle)
+                            .overlay(Circle().stroke(Color.gray, lineWidth: 5))
                         }
                     }
                     .padding()
                     .padding(.bottom, 100)
-                                        
-                    Text("Score - \(score) ")
-                        .font(.title2.weight(.semibold))
-                        .foregroundStyle(.white)
+                    
+                    Text("Completed Turns: \(turn)/10")
+                        .title2Style()
+                    
+                    Text("Score: \(score)")
+                        .title2Style()
                     
                     Spacer()
                     Spacer()
@@ -116,6 +161,13 @@ struct ContentView: View {
                             .accessibilityAddTraits(.isHeader)
                     }
                 }
+            }
+            .alert("Score = \(score)", isPresented: $showAlert) {
+                Button("Restart", role: .cancel) {
+                    restartGame()
+                }
+            } message: {
+                Text("Can you do better?")
             }
         }
             
