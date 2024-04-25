e912be7c3057325680e69f9256798a9b5c2c3d49
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-04-25 13:45:20 -0400

    Advanced String Practice
    components(seperatedBy: ), randomElement(), trimmingCharacters(in: ), UITextChecker, NSRange(), rangeOfMisspelledWord()

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -34,6 +34,27 @@ struct ContentView: View {
             }
         }
     }
+    
+    func testStrings() {
+        let test = "a b c"
+        
+        let letters = test.components(separatedBy: " ")
+        let letter = letters.randomElement()
+        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+    
+    func spellChecker() {
+        // Declare word and a checker to check for mispelling
+        let word = "swift"
+        let checker = UITextChecker()
+        
+        // Determine the range to check and the mispelled range
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let mispelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        // In the event NSNotFound is at the first index, there is no mispelling
+        let allGood =  mispelledRange.location == NSNotFound
+    }
 }
 
 #Preview {
