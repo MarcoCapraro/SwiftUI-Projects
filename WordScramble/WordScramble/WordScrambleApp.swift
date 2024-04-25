3ef72756d0eab14c50ebe48079710135937300eb
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-04-25 12:59:45 -0400

    Added WordScramble Project

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Marco Capraro on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
