ec25acd44c7e3e7248e00dfc00e0f40e33c7e37d
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-04-26 13:50:57 -0400

    Basic Animation Practice

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,14 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationScale = 1.0    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button("Tap Me") {
+            animationScale == 3.0 ? (animationScale = 1) : (animationScale += 0.25)
         }
-        .padding()
+        .padding(25)
+        .background(.black)
+        .foregroundStyle(.white)
+        .clipShape(.capsule)
+        .scaleEffect(animationScale)
+        .animation(.default, value: animationScale)
     }
 }
 
