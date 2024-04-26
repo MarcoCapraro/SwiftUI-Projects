9ffcab3ef3fea2a5158b4432aed5b668e52a03e1
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-04-26 13:29:25 -0400

    Added Animations Project

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Marco Capraro on 4/26/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
