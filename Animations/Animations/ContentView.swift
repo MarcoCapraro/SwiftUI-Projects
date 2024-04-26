552f1d9362bf95ba8ad3c54511c83cae0cd8855d
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-04-26 15:15:29 -0400

    Explicit Animations Practice

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -10,49 +10,50 @@ import SwiftUI
 struct ContentView: View {
     @State private var animationScale = 1.0
     @State private var animationPulse = 1.0
+    @State private var animationAmount = 0.0
     
     var body: some View {
         Spacer()
         
         Stepper("Scale Amount", value: $animationScale.animation(), in: 1...2, step: 0.25)
-            .frame(maxWidth: 300)
+            .frame(maxWidth: 175)
             .padding(20)
             .foregroundStyle(.black)
             .clipShape(.rect(cornerRadius: 20))
             .overlay(
                 RoundedRectangle(cornerRadius: 20)
                     .stroke(.black, lineWidth: 5)
+                    .scaleEffect(animationPulse)
+                    .opacity(1.5 - animationPulse)
+                    .animation(
+                        .easeOut(duration: 1.0)
+                            .repeatForever(autoreverses: false),
+                        value: animationPulse
+                    )
             )
+            .onAppear {
+                animationPulse = 1.5
+            }
         
         Spacer()
         
-        Button("Tap Me") {
+        Button("Spin Me") {
             animationScale == 2.0 ? (animationScale = 1) : (animationScale += 0.25)
+            withAnimation(.spring(duration: 1, bounce: 0.8)) {
+                animationAmount += 360
+            }
         }
         .padding(25)
         .background(.black)
         .foregroundStyle(.white)
         .clipShape(.capsule)
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: -1.0, y: 0.0, z: 0.0))
         .scaleEffect(animationScale)
         .blur(radius: 2.0 * (1 / animationScale))
         .animation(
             .spring(duration: 0.5, bounce: 0.75),
             value: animationScale
         )
-        .overlay(
-            Capsule()
-                .stroke(.red, lineWidth: 3.0)
-                .scaleEffect(animationPulse)
-                .opacity(2 - animationPulse)
-                .animation(
-                    .easeOut(duration: 1)
-                        .repeatForever(autoreverses: false),
-                    value: animationPulse
-                )
-        )
-        .onAppear {
-            animationPulse = 2
-        }
         
         Spacer()
     }
