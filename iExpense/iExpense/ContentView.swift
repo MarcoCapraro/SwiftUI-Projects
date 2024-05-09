8fcadd0b9f04abab8121ff4625b45634e19cf61e
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-05-09 17:26:29 -0400

    Final Polish of iExpense

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ExpenseItem: Identifiable, Codable {
+struct ExpenseItem: Identifiable, Codable, Equatable {
     var id = UUID()
     let name: String
     let type: String
@@ -25,6 +25,14 @@ class Expenses {
         }
     }
     
+    var personalItems: [ExpenseItem] {
+            items.filter { $0.type == "Personal" }
+        }
+        
+    var businessItems: [ExpenseItem] {
+        items.filter { $0.type == "Business" }
+    }
+    
     init() {
         if let savedItems = UserDefaults.standard.data(forKey: "Items") {
             if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
@@ -38,6 +46,36 @@ class Expenses {
     
 }
 
+struct ExpenseView: View {
+    let title: String
+    let expenses: [ExpenseItem]
+    let deleteItems: (IndexSet) -> Void
+    
+    var body: some View {
+        Section(title) {
+            ForEach(expenses) { item in
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text(item.name)
+                            .font(.headline)
+                        Text(item.type)
+                            .font(.subheadline)
+                    }
+                    
+                    Spacer()
+
+                    Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .foregroundStyle((item.amount < 100) ? ((item.amount < 10) ? .gray : .orange) : .red)
+                        .font(.headline.weight(.medium))
+                        .underline()
+                }
+            }
+            .onDelete(perform: deleteItems)
+        }
+        
+    }
+}
+
 struct ContentView: View {
     @State private var expenses = Expenses()
     @State private var isShowingAddView = false
@@ -45,21 +83,9 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             List {
-                ForEach(expenses.items) { item in
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text(item.name)
-                                .font(.headline)
-                            Text(item.type)
-                                .font(.subheadline)
-                        }
-                        
-                        Spacer()
-
-                        Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                    }
-                }
-                .onDelete(perform: removeExpense)
+                ExpenseView(title: "Personal", expenses: expenses.personalItems, deleteItems: removePersonalItems)
+                ExpenseView(title: "Business", expenses: expenses.businessItems, deleteItems: removeBusinessItems)
+                
             }
             .navigationTitle("iExpense")
             .toolbar {
@@ -73,9 +99,27 @@ struct ContentView: View {
         }
     }
     
-    func removeExpense(at offsets: IndexSet) {
-        expenses.items.remove(atOffsets: offsets)
-    }
+    func removeItems(at offsets: IndexSet, in inputArray: [ExpenseItem]) {
+         var objectsToDelete = IndexSet()
+         
+         for offset in offsets {
+             let item = inputArray[offset]
+             
+             if let index = expenses.items.firstIndex(of: item) {
+                 objectsToDelete.insert(index)
+             }
+         }
+         
+         expenses.items.remove(atOffsets: objectsToDelete)
+     }
+     
+     func removePersonalItems(at offsets: IndexSet) {
+         removeItems(at: offsets, in: expenses.personalItems)
+     }
+     
+     func removeBusinessItems(at offsets: IndexSet) {
+         removeItems(at: offsets, in: expenses.businessItems)
+     }
     
 }
 
