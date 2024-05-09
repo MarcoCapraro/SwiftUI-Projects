0c2e47e89e5926c70fd1da8cfd123ce5d86cfdea
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-05-09 16:20:30 -0400

    Created View to Add Expense Details; UserDefault Persistence for Expense Tracking

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,7 +7,8 @@
 
 import SwiftUI
 
-struct ExpenseItem: Hashable {
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
     let name: String
     let type: String
     let amount: Double
@@ -15,26 +16,59 @@ struct ExpenseItem: Hashable {
 
 @Observable
 class Expenses {
-    var items = [ExpenseItem]()
+    var items = [ExpenseItem]() {
+        // Whenever items is appended / deleted from, encode the new list
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+        
+        items = []
+    }
+    
 }
 
 struct ContentView: View {
     @State private var expenses = Expenses()
+    @State private var isShowingAddView = false
     
     var body: some View {
         NavigationStack {
             List {
-                ForEach(expenses.items, id: \.self) { item in
-                    Text("\(item.name)")
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                                .font(.subheadline)
+                        }
+                        
+                        Spacer()
+
+                        Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    }
                 }
                 .onDelete(perform: removeExpense)
             }
             .navigationTitle("iExpense")
             .toolbar {
                 Button("Add Expense", systemImage: "plus") {
-                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
-                    expenses.items.append(expense)
+                    isShowingAddView = true
                 }
+            }
+            .sheet(isPresented: $isShowingAddView) {
+                AddView(expenses: expenses)
             }
         }
     }
