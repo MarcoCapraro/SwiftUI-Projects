8bd2a702dd9e39cd41be411fa56143a4edbf8ace
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-05-13 16:43:27 -0400

    ScrollView and Lazy H/V Stacks

--- MoonShot/MoonShot/ContentView.swift
+++ MoonShot/MoonShot/ContentView.swift
@@ -7,14 +7,40 @@
 
 import SwiftUI
 
+struct CustomText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+    }
+    
+    init(text: String) {
+        print("Creating new CustomText")
+        self.text = text
+    }
+}
+
 struct ContentView: View {
     var body: some View {
+        
         Image(.tron)
             .resizable()
             .scaledToFit()
             .containerRelativeFrame(.horizontal) { size, axis in
                 size * 0.8
             }
+        
+        ScrollView(.horizontal) {
+            LazyHStack {
+                ForEach(0..<100) {
+                    CustomText(text: "Item \($0)")
+                        .font(.title)
+                }
+            }
+        }
+        
+        
+        
     }
 }
 
