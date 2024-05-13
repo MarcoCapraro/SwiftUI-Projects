5d95092d85812b8f87dde2992d99da77486ce493
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-05-13 12:06:09 -0400

    Added MoonShot Project

---
+++ MoonShot/MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Marco Capraro on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
