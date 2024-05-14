154cb329c40360b4c26fffa06f8cc7f0d9f7f9ae
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-05-14 12:00:55 -0400

    Decoding Custom Data from JSON with proper Error Catching

---
+++ MoonShot/MoonShot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  MoonShot
+//
+//  Created by Marco Capraro on 5/14/24.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
--- MoonShot/MoonShot/ContentView.swift
+++ MoonShot/MoonShot/ContentView.swift
@@ -7,89 +7,11 @@
 
 import SwiftUI
 
-struct CustomText: View {
-    var text: String
-    
-    var body: some View {
-        Text(text)
-    }
-    
-    init(text: String) {
-        print("Creating new CustomText")
-        self.text = text
-    }
-}
-
-struct User: Codable {
-    let name: String
-    let address: Address
-}
-
-struct Address: Codable {
-    let street: String
-    let city: String
-}
-
 struct ContentView: View {
-    let layout = [
-        GridItem(.adaptive(minimum: 80, maximum: 120))
-    ]
+    let astronauts = Bundle.main.decode("astronauts.json")
     
     var body: some View {
-        NavigationStack {
-            Image(.tron)
-                .resizable()
-                .scaledToFit()
-                .containerRelativeFrame(.horizontal) { size, axis in
-                    size * 0.8
-                }
-            
-            List(0..<100) { row in
-                NavigationLink("Row \(row)") {
-                    ScrollView {
-                        LazyVGrid(columns: layout) {
-                            ForEach(0..<1000) {
-                                Text("Item \($0)")
-                            }
-                        }
-                    }
-                }
-            }
-            .navigationTitle("SwiftUI")
-            
-            ScrollView(.horizontal) {
-                LazyHStack {
-                    ForEach(0..<100) {
-                        CustomText(text: "Item \($0)")
-                            .font(.title)
-                    }
-                }
-            }
-            
-            Button("Decode JSON") {
-                // JSON String to test with
-                let input = """
-                {
-                    "name": "Marco Capraro",
-                    "address": {
-                        "street": "555, Marco Capraro Avenue",
-                        "city": "Nashville"
-                    }
-                }
-                """
-                
-                let data = Data(input.utf8)
-                let decoder = JSONDecoder()
-                
-                if let user = try? decoder.decode(User.self, from: data) {
-                    print(user.address.street)
-                }
-            }
-        }
-        
-        
-        
-        
+        Text("Hello World")
         
     }
 }
