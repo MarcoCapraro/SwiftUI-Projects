abe77ccd4d1c64c674d3493402a8341c428efc6c
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-05-16 15:55:10 -0400

    Different ways of navigating screens

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -7,6 +7,12 @@
 
 import SwiftUI
 
+struct Student: Hashable {
+    let id = UUID()
+    let name: String
+    let age: Int
+}
+
 struct DetailView: View {
     var number: Int
     
@@ -23,10 +29,14 @@ struct DetailView: View {
 struct ContentView: View {
     var body: some View {
         NavigationStack {
-            List(0..<1000) { i in
-                NavigationLink("Tap Me") {
-                    DetailView(number: i)
-                }
+            List(0..<100) { i in
+                NavigationLink("Select \(i)", value: i)
+            }
+            .navigationDestination(for: Int.self) { selection in
+                Text("You selected \(selection)")
+            }
+            .navigationDestination(for: Student.self) { student in
+                Text("You selected \(student.name)")
             }
         }
     }
