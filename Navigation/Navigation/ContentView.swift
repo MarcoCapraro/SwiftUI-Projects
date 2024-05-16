57f8ddd6337f25c993e9265d235a777ff68bb72e
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-05-16 16:06:45 -0400

    Programmatic Navigation

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -27,16 +27,25 @@ struct DetailView: View {
 }
 
 struct ContentView: View {
+    @State private var path = [Int]()
+    
     var body: some View {
-        NavigationStack {
-            List(0..<100) { i in
-                NavigationLink("Select \(i)", value: i)
+        NavigationStack(path: $path) {
+            VStack {
+                Button("Show 32") {
+                    path = [32]
+                }
+                
+                Button("Show 64") {
+                    path.append(64)
+                }
+                
+                Button("Show 64 then 32") {
+                    path = [32, 64]
+                }
             }
             .navigationDestination(for: Int.self) { selection in
                 Text("You selected \(selection)")
-            }
-            .navigationDestination(for: Student.self) { student in
-                Text("You selected \(student.name)")
             }
         }
     }
