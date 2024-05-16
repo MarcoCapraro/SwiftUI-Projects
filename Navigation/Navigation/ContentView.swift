43b710d6345f6cb6e2e78e1f67746ac0b1dd3535
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-05-16 16:31:27 -0400

    Setup root view return via programmatic Button

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -15,37 +15,77 @@ struct Student: Hashable {
 
 struct DetailView: View {
     var number: Int
-    
-    init(number: Int) {
-        self.number = number
-        print("Creating DetailView")
-    }
-    
+    @Binding var path: NavigationPath
+        
     var body: some View {
-        Text("\(number)")
+        NavigationLink("Go to Random Number", value: Int.random(in: 1..<1000))
+            .navigationTitle("Number \(number)")
+            .toolbar {
+                Button("Home") {
+                    path = NavigationPath()
+                }
+            }
     }
 }
 
 struct ContentView: View {
-    @State private var path = [Int]()
+    @State private var path1 = [Int]()
+    @State private var path2 = NavigationPath()
+    @State private var path3 = NavigationPath()
     
     var body: some View {
-        NavigationStack(path: $path) {
-            VStack {
-                Button("Show 32") {
-                    path = [32]
+        VStack {
+            NavigationStack(path: $path3) {
+                DetailView(number: 0, path: $path3)
+                    .navigationDestination(for: Int.self) { i in
+                        DetailView(number: i, path: $path3)
+                    }
+            }
+            
+            NavigationStack(path: $path2) {
+                List {
+                    ForEach(0..<5) { i in
+                        NavigationLink("Select Number \(i)", value: i)
+                    }
+                    
+                    ForEach(0..<5) { i in
+                        NavigationLink("Select String \(i)", value: String(i))
+                    }
                 }
-                
-                Button("Show 64") {
-                    path.append(64)
+                .toolbar {
+                    Button("Push 556") {
+                        path2.append(556)
+                    }
+                    
+                    Button("Push Hello") {
+                        path2.append("Hello")
+                    }
                 }
-                
-                Button("Show 64 then 32") {
-                    path = [32, 64]
+                .navigationDestination(for: Int.self) { selection in
+                    Text("You selected Number \(selection)")
+                }
+                .navigationDestination(for: String.self) { selection in
+                    Text("You selected String \(selection)")
                 }
             }
-            .navigationDestination(for: Int.self) { selection in
-                Text("You selected \(selection)")
+            
+            NavigationStack(path: $path1) {
+                VStack {
+                    Button("Show 32") {
+                        path1 = [32]
+                    }
+                    
+                    Button("Show 64") {
+                        path1.append(64)
+                    }
+                    
+                    Button("Show 64 then 32") {
+                        path1 = [32, 64]
+                    }
+                }
+                .navigationDestination(for: Int.self) { selection in
+                    Text("You selected \(selection)")
+                }
             }
         }
     }
