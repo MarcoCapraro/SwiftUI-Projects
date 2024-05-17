f86f250a89b4eb8b7626450fb01d2f8152b433a9
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-05-17 14:03:03 -0400

    toolbar Button Placements

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -11,16 +11,28 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationStack {
-            List(0..<100) { i in
-                NavigationLink("Row \(i)") {
-                    Text("Detail View")
+            VStack {
+                List(0..<100) { i in
+                    NavigationLink("Row \(i)") {
+                        Text("Detail View")
+                    }
+                }
+                .navigationTitle("Title Customization")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbarBackground(.blue)
+                .toolbarColorScheme(.dark)
+                // .toolbar(.hidden, for: .navigationBar)
+            }
+            .toolbar {
+                ToolbarItemGroup(placement: .topBarTrailing) {
+                    Button("B1") {}
+                    Button("B2") {}
+                }
+                
+                ToolbarItem(placement: .bottomBar) {
+                    Button("Tap Me") {}
                 }
             }
-            .navigationTitle("Title Customization")
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbarBackground(.blue)
-            .toolbarColorScheme(.dark)
-            // .toolbar(.hidden, for: .navigationBar)
         }
     }
 }
