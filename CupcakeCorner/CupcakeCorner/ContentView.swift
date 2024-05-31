1937d66fb9945a61b33fc3af57a82b7de11afb2b
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-05-31 13:43:12 -0400

    Using URLSession, async, await, and Codable to download API Data asynchronously

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -7,15 +7,53 @@
 
 import SwiftUI
 
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
 struct ContentView: View {
+    @State private var results = [Result]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(results, id: \.trackId) { result in
+            VStack(alignment: .leading) {
+                Text(result.trackName)
+                    .font(.headline)
+                Text(result.collectionName)
+            }
         }
-        .padding()
+        .task {
+            await loadData()
+        }
+    }
+    
+    // async keyword tells swift function might sleep before finishing
+    func loadData() async {
+        // 1.Create URL to read
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        
+        // 2. Fetch data from that URL
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            // 3. Decode result into response struct
+            if let decodedData = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedData.results
+            }
+        } catch {
+            print("Invalid Data")
+        }
+        
+        
     }
 }
 
