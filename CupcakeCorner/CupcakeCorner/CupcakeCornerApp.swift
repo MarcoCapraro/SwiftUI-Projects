ac984de433b14dfbf8f160eba3c74207b81627f5
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-05-31 13:25:07 -0400

    Created new project CupcakeCorner

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Marco Capraro on 5/31/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
