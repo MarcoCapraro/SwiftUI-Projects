c8c2505cfb7b4ca6e13950cfa467048dc64efb6b
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-06-06 16:05:07 -0400

    Added New Project BookWorm

---
+++ BookWorm/BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Marco Capraro on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
