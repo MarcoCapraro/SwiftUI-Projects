e23c7355bfa6b3f51ed6d992f5dd1634da69051a
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-07-10 15:47:32 -0400

    Created a DetailView, Present DetailView of Book when Tapped within List
    DetailView provides the context of the book (title, author, genre) and shows the review and rating left by the User who saved it to their library.

---
+++ BookWorm/BookWorm/DetailView.swift
@@ -0,0 +1,57 @@
+//
+//  DetailView.swift
+//  BookWorm
+//
+//  Created by Marco Capraro on 7/10/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct DetailView: View {
+    let book: Book
+    
+    var body: some View {
+        ScrollView {
+            ZStack(alignment: .bottomTrailing) {
+                Image(book.genre)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(book.genre.uppercased())
+                    .fontWeight(.black)
+                    .padding(8)
+                    .foregroundStyle(.white)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
+                    .offset(x: -5, y: -5)
+            }
+            
+            Text(book.author)
+                .font(.title)
+                .foregroundStyle(.secondary)
+            
+            Text(book.review)
+                .padding()
+            
+            RatingView(rating: .constant(book.rating))
+                .font(.largeTitle)
+        }
+        .navigationTitle(book.title)
+        .navigationBarTitleDisplayMode(.inline)
+        .scrollBounceBehavior(.basedOnSize)
+    }
+}
+
+#Preview {
+    do {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try ModelContainer(for: Book.self, configurations: config)
+        let example = Book(title: "Title", author: "Author", genre: "Fantasy", review: "Review", rating: 4)
+        
+        return DetailView(book: example)
+            .modelContainer(container)
+    } catch {
+        return Text("Failed to create Preview: \(error.localizedDescription)")
+    }
+}
