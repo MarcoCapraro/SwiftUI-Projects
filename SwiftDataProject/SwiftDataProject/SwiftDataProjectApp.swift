beeef573878b42455b8fe09241945c1cfcf3aaaa
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-07-12 14:07:07 -0400

    Added SwiftDataProject

---
+++ SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Marco Capraro on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
