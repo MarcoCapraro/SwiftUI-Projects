263ce0da3c325ba29beb27ffdb457718f706b4ce
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-07-12 14:37:37 -0400

    Added a EditUserView and Created a NavigationStack
    ContentView will create a NavigationStack, presenting a List of all created Users. When a User NavigationLink is selected it redirects to that users EditUserView. In the event the Add User + button is pressed it will immediately create a blank user and redirect the path of the Navigation Stack to that blank users EditUserView.

--- SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
+++ SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -5,6 +5,7 @@
 //  Created by Marco Capraro on 7/12/24.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct SwiftDataProjectApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: User.self)
     }
 }
---
+++ SwiftDataProject/SwiftDataProject/User.swift
@@ -0,0 +1,23 @@
+//
+//  User.swift
+//  SwiftDataProject
+//
+//  Created by Marco Capraro on 7/12/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class User {
+    var name: String
+    var city: String
+    var joinDate: Date
+    
+    init(name: String, city: String, joinDate: Date) {
+        self.name = name
+        self.city = city
+        self.joinDate = joinDate
+    }
+    
+}
