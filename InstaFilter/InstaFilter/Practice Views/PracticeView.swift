d354c6c468d89ec4d0f9d2ce9867378f9194f77b
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-07-19 15:09:30 -0400

    Created a PracticeView to Practice PropertyWrappers, onChange, and confirmationDialog()

---
+++ InstaFilter/InstaFilter/Practice Views/PracticeView.swift
@@ -0,0 +1,67 @@
+//
+//  PracticeView.swift
+//  InstaFilter
+//
+//  Created by Marco Capraro on 7/19/24.
+//
+
+
+import SwiftUI
+
+struct PracticeView: View {
+    @State private var blurAmount = 0.0
+//      Doesn't work with slider binded value, need to write onChange function
+//        didSet {
+//            print("New Value is \(blurAmount)")
+//        }
+    
+    @State private var showConfirmation = false
+    @State private var backgroundColor = Color.white
+    
+    var body: some View {
+        VStack {
+            
+            VStack {
+                Text("Hello, world!")
+                    .blur(radius: blurAmount)
+                
+                Slider(value: $blurAmount, in: 0...20)
+                    .onChange(of: blurAmount) { oldValue, newValue in
+                        print("New Value is \(blurAmount)")
+                    }
+                
+                
+                Button("Random Blur") {
+                    blurAmount = Double.random(in: 0...20)
+                }
+            }
+            
+            Spacer()
+            
+            VStack {
+                Button("Hello World") {
+                    showConfirmation.toggle()
+                }
+                .frame(width: 300, height: 300)
+                .background(backgroundColor)
+                .clipShape(.capsule)
+                .confirmationDialog("Change Background", isPresented: $showConfirmation) {
+                    Button("Red") { backgroundColor = .red }
+                    Button("Green") { backgroundColor = .green }
+                    Button("Blue") { backgroundColor = .blue }
+                    Button("Cancel", role: .cancel) {}
+                } message: {
+                    Text("Select a new color")
+                }
+            }
+            
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    PracticeView()
+}
+
