14d3c545e1af464a0f58115efdc2dbbef2fd7e18
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-07-24 14:19:13 -0400

    Updated UI of InstaFilter

--- InstaFilter/InstaFilter/ContentView.swift
+++ InstaFilter/InstaFilter/ContentView.swift
@@ -9,11 +9,47 @@ import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
     
     var body: some View {
-        VStack {
-            Text("Hello, World")
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                if let processedImage {
+                    processedImage
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                }
+            
+                
+                Spacer()
+                
+                HStack {
+                    Text("Intensity")
+                    // Intensity Slider 0-1
+                    Slider(value: $filterIntensity)
+                }
+                
+                HStack {
+                    Button("Change Filter", action: changeFilter)
+                    
+                    Spacer()
+                    
+                    // Sharing Button
+
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("InstaFilter")
         }
+    }
+    
+    func changeFilter() {
+        
     }
 }
 
