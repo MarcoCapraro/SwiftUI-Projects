49cdd908c033f0f5010c79c000e887a5936ad01a
Marco Capraro <53179095+MarcoCapraro@users.noreply.github.com>
2024-08-26 14:25:40 -0400

    Added AccessibilitySandbox Project and Added Voiceover Accessibility Label

---
+++ AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Marco Capraro on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
